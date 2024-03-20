6008a783740e38f575c01884630b048adba768ed
jaroslaw-filipiak <info@j-filipiak.pl>
2024-03-20 18:33:17 +0100

    .env tet2

--- stores/main.js
+++ stores/main.js
@@ -16,7 +16,9 @@ export const useMainStore = defineStore('mainStore', {
       const apiUrl = useRuntimeConfig().public.apiBaseUrl;
       console.log(apiUrl);
       try {
-        const response = await fetch(`${apiUrl}/wp-json/options/pricing`);
+        const response = await fetch(
+          `https://www.${apiUrl}/wp-json/options/pricing`
+        );
         const data = await response.json();
 
         this.pricing = data;
