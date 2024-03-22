146b568bf9fdb5fd26337fb261cc2b4716983f40
jaroslaw-filipiak <info@j-filipiak.pl>
2024-03-22 13:13:56 +0100

    update baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: '/clients/grafikonline/safety-area/',
+    // baseURL: '/clients/grafikonline/safety-area/',
     // buildAssetsDir: '/mydir/_nuxt/',
   },
   devtools: { enabled: true },
